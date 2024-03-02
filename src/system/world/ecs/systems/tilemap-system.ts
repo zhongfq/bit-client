a878fdfca22ad37971bb5f2983f77f526642e3a8
猴大人 <249941679@qq.com>
2024-03-02 11:43:58 +0800

    大地图-支持地图分块和视野加载

--- src/system/world/ecs/systems/tilemap-system.ts
+++ src/system/world/ecs/systems/tilemap-system.ts
@@ -1,9 +1,24 @@
 import { ecs } from "../../../../core/ecs";
 import { StringUtil } from "../../../../core/utils/string-util";
 import { WorldContext } from "../../world-context";
+import { CameraComponent } from "../components/camera-component";
+import { TransformComponent } from "../components/movement-component";
 import { Tilemap, TilemapComponent } from "../components/tilemap-component";
 
 export class TilemapSystem extends ecs.System {
+
+    private _world!: Tilemap.World;
+
+    private static readonly TICK = 200;
+    private _lastTime: number = 0;
+
+    private _lastRect: Laya.Rectangle = Laya.Rectangle.create();
+    private _curRect: Laya.Rectangle = Laya.Rectangle.create();
+
+    private _addArr: string[] = [];
+    private _delArr: string[] = [];
+
+    private _totalMap: Map<string, Map<string, Tilemap.Element>> = new Map();
 
     constructor(readonly context: WorldContext) {
         super();
@@ -11,22 +26,32 @@ export class TilemapSystem extends ecs.System {
     }
 
     private async _init() {
-        const worldMap = await Laya.loader.fetch("resources/data/world-map/world-map1.json", "json") as Tilemap.WorldMap;
-        worldMap.layers.forEach(layer => {
-            if (!layer.data) {
-                return;
-            }
-            switch (layer.name) {
-                case Tilemap.LayerName.Ground:
-                    this._initGround(worldMap, layer);
-                    break;
-                case Tilemap.LayerName.Static:
-                    this._initStatic(worldMap, layer);
-                    break;
-                default:
-                    break;
-            }
-        })
+        if (!this._world) {
+            this._world = await Laya.loader.fetch("resources/data/world-map/world.json", "json") as Tilemap.World;
+            this._world.maps.forEach(info => {
+                [info.x, info.y] = this._transMapPos(info);
+                [info.width, info.height] = [TilemapComponent.MAP_WIDTH, TilemapComponent.MAP_HEIGHT];
+
+                console.log(info.fileName, info.x, info.y);
+            });
+        }
+    }
+
+    private _transMapPos(info: Tilemap.MapInfo) {
+        const scale = Math.sqrt((TilemapComponent.TILE_HEIGHT * 2) ** 2 / 2);
+
+        const mat = new Laya.Matrix3x3();
+        mat.translate(new Laya.Vector2((TilemapComponent.MAP_HEIGHT * TilemapComponent.TILE_WIDTH) / 2, 0), mat);
+        mat.scale(new Laya.Vector2(1, 0.5), mat);
+        mat.rotate(Math.PI / 4, mat);
+        mat.scale(new Laya.Vector2(scale, scale), mat);
+
+        const invertMat = new Laya.Matrix3x3();
+        mat.invert(invertMat);
+
+        const pos = new Laya.Vector2(info.x + info.width / 2, info.y);
+        Laya.Vector2.transformCoordinate(pos, invertMat, pos);
+        return [pos.x, pos.y];
     }
 
     private async _initGround(worldMap: Tilemap.WorldMap, layer: Tilemap.Layer) {
@@ -103,6 +128,116 @@ export class TilemapSystem extends ecs.System {
     }
 
     update(dt: number): void {
+        const curTime = Laya.timer.currTimer;
+        if (curTime - this._lastTime < TilemapSystem.TICK) {
+            return;
+        }
+        this._lastTime = curTime;
 
+        const camera = this.ecs.getSingletonComponent(CameraComponent)!;
+        const transform = this.ecs.getComponent(camera.focus as number, TransformComponent)!;
+        const curX = Math.floor(transform.position.x - TilemapComponent.VISION_WIDTH / 2);
+        const curY = Math.floor(transform.position.z - TilemapComponent.VISION_HEIGHT / 2);
+        this._curRect.setTo(curX, curY, TilemapComponent.VISION_WIDTH, TilemapComponent.VISION_HEIGHT);
+
+        if (this._curRect.equals(this._lastRect)) {
+            return;
+        }
+
+        this._addArr = [];
+        this._delArr = [];
+
+        if (!this._lastRect.isEmpty()) {
+            for (let x = curX; x < curX + this._curRect.width; x++) {
+                for (let y = curY; y < curY + this._curRect.height; y++) {
+                    if (!this._lastRect.contains(x, y)) {
+                        this._addArr.push(TilemapComponent.XY_TO_KEY(x, y));
+                    }
+                }
+            }
+            const [lastX, lastY] = [this._lastRect.x, this._lastRect.y];
+            for (let x = lastX; x < lastX + this._lastRect.width; x++) {
+                for (let y = lastY; y < lastY + this._lastRect.height; y++) {
+                    if (!this._curRect.contains(x, y)) {
+                        this._delArr.push(TilemapComponent.XY_TO_KEY(x, y));
+                    }
+                }
+            }
+        } else {
+            for (let x = curX; x < curX + this._curRect.width; x++) {
+                for (let y = curY; y < curY + this._curRect.height; y++) {
+                    this._addArr.push(TilemapComponent.XY_TO_KEY(x, y));
+                }
+            }
+        }
+        this._lastRect.copyFrom(this._curRect);
+
+        for (let i = 0; i < this._addArr.length; i++) {
+            const key = this._addArr[i];
+            this._tryAdd(key);
+        }
+
+        for (let i = 0; i < this._delArr.length; i++) {
+            const key = this._delArr[i];
+            this._tryDel(key);
+        }
+    }
+
+    private _tryAdd(key: string) {
+        const [x, y] = TilemapComponent.KEY_TO_XY(key);
+
+        let elements = this._totalMap.get(key);
+        if (!elements) {
+            elements = new Map();
+            this._totalMap.set(key, elements);
+        }
+
+        this._world.maps.forEach(async (info: Tilemap.MapInfo) => {
+
+            if (info.x <= x && x < info.x + info.width &&
+                info.y <= y && y < info.y + info.height) {
+
+                if (!info.worldMap) {
+                    const realFileName = info.fileName.split('/').at(-1)?.replace('.tmx', '.json');
+                    info.worldMap = await Laya.loader.fetch("resources/data/world-map/" + realFileName, "json") as Tilemap.WorldMap;
+                }
+
+                const idx = (y - info.y) * info.width + (x - info.x);
+                info.worldMap.layers.forEach(layer => {
+                    if (!layer.data) {
+                        return;
+                    }
+                    const gid = layer.data[idx];
+
+                    let element: Tilemap.Element | undefined = undefined;
+                    switch (layer.name) {
+                        case Tilemap.LayerName.Ground:
+                            element = Tilemap.Element.create("Tilemap.GroundElement", Tilemap.GroundElement);
+                            break;
+                        case Tilemap.LayerName.Static:
+                            element = Tilemap.Element.create('Tilemap.StaticElement', Tilemap.StaticElement);
+                            break;
+                        default:
+                            break;
+                    }
+                    if (element) {
+                        element.init(x, y, gid);
+                        element.draw(this.context);
+                        elements?.set(layer.name, element);
+                    }
+                })
+            }
+        })
+    }
+
+    private _tryDel(key: string) {
+        const elements = this._totalMap.get(key);
+        if (elements) {
+            elements.forEach((element) => {
+                element.recover();
+            })
+            elements.clear();
+        }
+        this._totalMap.delete(key);
     }
 }
