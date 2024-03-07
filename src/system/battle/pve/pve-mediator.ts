30d6704c3cd55cfd1fe92a435528eda625f17da1
zhangxinpeng <541779390@qq.com>
2024-03-07 17:20:14 +0800

    世界增加监听调取gm面板事件，gm修改显示信息错误

--- src/system/battle/pve/pve-mediator.ts
+++ src/system/battle/pve/pve-mediator.ts
@@ -1,4 +1,6 @@
+import { app } from "../../../app";
 import { Mediator } from "../../../core/ui-mediator";
+import { ui } from "../../../misc/ui";
 import { PveUI } from "../../../ui-runtime/scene/PveUI";
 
 @Laya.regClass()
@@ -6,4 +8,10 @@ export class PveMediator extends Mediator {
     declare owner: PveUI;
 
     override onAwake() {}
+
+    override onKeyDown(evt: Laya.Event): void {
+        if (evt.ctrlKey && evt.keyCode == Laya.Keyboard.B) {
+            app.ui.show(ui.GM);
+        }
+    }
 }
