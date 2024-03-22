d3ade143e2318b3505664484b1823d97b03aed34
codetypes <codetypes@gmail.com>
2024-03-22 18:42:47 +0800

    支持通用加载

--- src/core/loader.ts
+++ src/core/loader.ts
@@ -1,54 +1,61 @@
 type ContextChecker = () => boolean;
+type ExecutorResolve = (value: unknown) => void;
 
 interface PromiseDescriptor {
-    resolve: (value: unknown) => void;
+    resolve: ExecutorResolve;
     contextChecker?: ContextChecker;
 }
 
 export class Loader {
     private _loadings: Map<string, PromiseDescriptor[]> = new Map();
 
-    /**
-     * 同 loadPrefab
-     * @param url
-     * @param checker
-     * @returns
-     */
     public async loadJson<T>(url: string, checker?: ContextChecker): Promise<T> {
-        const json = (await this._load(url, Laya.Loader.JSON, checker)) as Laya.TextResource;
+        const json = await this.load<Laya.TextResource>(url, Laya.Loader.JSON, checker);
         return json.data as T;
     }
 
     public async loadAtlas(url: string, checker?: ContextChecker) {
-        return this._load(url, Laya.Loader.ATLAS, checker) as Promise<Laya.AtlasResource>;
+        return this.load<Laya.AtlasResource>(url, Laya.Loader.ATLAS, checker);
     }
 
     public async loadTexture2D(url: string, checker?: ContextChecker) {
-        return this._load(url, Laya.Loader.TEXTURE2D, checker) as Promise<Laya.Texture2D>;
+        return this.load<Laya.Texture2D>(url, Laya.Loader.TEXTURE2D, checker);
+    }
+
+    public async loadPrefab(url: string, checker?: ContextChecker) {
+        return this.load<Laya.Prefab>(url, Laya.Loader.HIERARCHY, checker);
     }
 
     /**
-     * 加载预制体，并且有返回一定成功。
+     * 加载资源，并且有返回一定成功。
      * @param url 加载的资源。
      * @param checker 在加载成功的情况下并且通过检查才会返回。
      * @returns
      */
-    public async loadPrefab(url: string, checker?: ContextChecker) {
-        return this._load(url, Laya.Loader.HIERARCHY, checker) as Promise<Laya.Prefab>;
-    }
+    public load<T = unknown>(url: string, checker?: ContextChecker): Promise<T>;
 
-    private _load(url: string, type: string, checker?: ContextChecker): Promise<unknown> {
-        const res = Laya.loader.getRes(url);
+    public load<T = unknown>(url: string, type: string, checker?: ContextChecker): Promise<T>;
+
+    public load<T = unknown>(
+        url: string,
+        type?: string | ContextChecker,
+        checker?: ContextChecker
+    ): Promise<T> {
+        const res = Laya.loader.getRes(url) as T;
         if (res) {
             return Promise.resolve(res);
         }
-        return new Promise((resolve) => {
+        return new Promise<T>((resolve) => {
             let arr = this._loadings.get(url);
             if (!arr) {
                 arr = [];
                 this._loadings.set(url, arr);
             }
-            arr.push({ resolve, contextChecker: checker });
+            if (typeof type === "function") {
+                checker = type;
+                type = undefined;
+            }
+            arr.push({ resolve: resolve as ExecutorResolve, contextChecker: checker });
             if (arr.length == 1) {
                 Laya.loader.load(url, type).then((value) => {
                     if (!value) {
